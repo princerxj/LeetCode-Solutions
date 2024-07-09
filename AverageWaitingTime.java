8d23a865ee48ccedccc2f69e5f7d2824dc8d07dd
princerxj <pr273582@gmail.com>
2024-07-09 12:40:34 +0530

    Solution for "Average Waiting Time"

---
+++ AverageWaitingTime.java
@@ -0,0 +1,22 @@
+public class AverageWaitingTime {
+    public static double averageWaitingTime(int[][] customers) {
+        int currentTime = 0;
+        double totalTime = 0;
+        for (int[] customer : customers) {
+            if (currentTime <= customer[0]) {
+                totalTime = totalTime + customer[1];
+                currentTime = customer[0] + customer[1];
+            } else {
+                totalTime = totalTime + (currentTime - customer[0]) + customer[1];
+                currentTime = currentTime + customer[1];
+            }
+        }
+        return totalTime / customers.length;
+    }
+
+    public static void main(String[] args) {
+        int[][] custoemrs = {{1,2},{2,5},{4,3}};
+        double ans = averageWaitingTime(custoemrs);
+        System.out.println(ans);
+    }
+}
